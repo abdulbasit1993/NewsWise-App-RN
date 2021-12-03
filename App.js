672a1a1e58b88fecfd0c2ca01ab3a9746d7ca85a
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2021-12-03 19:36:22 +0500

    Added News Screen with Carousel

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import Context from './src/API/Context';
 import NewsTabs from './src/components/NewsTabs';
 
-export default function App() {
+function App() {
   return (
     <View style={{...styles.container, backgroundColor: '#282C35'}}>
       <NewsTabs />
@@ -16,5 +17,13 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+export default () => {
+  return (
+    <Context>
+      <App />
+    </Context>
+  );
+};
 
 // Tutorial link: https://youtu.be/5Ko7B3G4U04
